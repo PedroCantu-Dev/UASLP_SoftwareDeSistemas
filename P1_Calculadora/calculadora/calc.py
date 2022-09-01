66adbcd39137ad29de64ff0cef29fc1c416da0f2
pedejeca135 <pjco90@hotmail.com>
2022-09-01 10:18:02 -0500

    añadi los tokens basicos para la caculadora

---
+++ P1_Calculadora/calculadora/calc.py
@@ -0,0 +1,22 @@
+import sys
+import ply.lex as lex
+import ply.yacc as yacc
+import sys
+
+
+tokens = [
+    'INT',
+    'FLOAT',
+    'NAME',
+    'PLUS',
+    'MINUS',
+    'DIVIDE',
+    'MULTIPLY',
+    'EQUALS'
+]
+
+t_PLUS = r'\+'
+t_MINUS = r'\-'
+t_MULTIPLY = r'\*'
+t_DIVIDE = r'\/'
+t_EQUALS = r'\='
